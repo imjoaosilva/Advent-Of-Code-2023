ae969fdc93f18a53165cebc62ad77ec794955ea4
Z4 <z4kreyyt@gmail.com>
2023-12-14 01:03:18 +0000

    Removing Files

--- rust/day01/target/debug/deps/day01-8488317a9e4f0264.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Z4\Desktop\AdventOfCode\rust\day01\target\debug\deps\libday01-8488317a9e4f0264.rmeta: src\main.rs
-
-C:\Users\Z4\Desktop\AdventOfCode\rust\day01\target\debug\deps\day01-8488317a9e4f0264.d: src\main.rs
-
-src\main.rs:
