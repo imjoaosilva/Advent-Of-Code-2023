b01bf450623aea804b3d63cf1e5d343a88c3c159
Z4 <z4kreyyt@gmail.com>
2023-12-19 22:35:04 +0000

    Ending Part 01 Day 01

--- rust/day01/src/main.rs
+++ rust/day01/src/main.rs
@@ -1,7 +1,8 @@
+use std::fs;
+
 trait Challenge {
     fn new(initial_path: String, input_path: String) -> Self;
-    fn run_tests(&self) -> ();
-    fn run(&self) -> ();
+    fn run(&self, test: bool) -> ();
 }
 
 struct Part01 {
@@ -17,16 +18,41 @@ impl Challenge for Part01 {
         }
     }
 
-    fn run_tests(&self) {
-        println!("Running tests in: {}", self.initial_path)
-    }
+    fn run(&self, test: bool) {
+        let path = if test {
+            &self.initial_path
+        } else {
+            &self.input_path
+        };
 
-    fn run(&self) {
-        println!("Running code in: {}", self.input_path)
+        let codes = fs
+            ::read_to_string(path)
+            .expect("Something went wrong reading the file");
+
+        let result = codes.lines().fold(0, |acc, code| {
+            let code_numbers: Vec<i32> = code
+                .split("")
+                .map(|x| x.parse().unwrap_or(-1))
+                .filter(|&num| num != -1)
+                .collect();
+
+            let number = [code_numbers[0], code_numbers[code_numbers.len() - 1]]
+                .iter()
+                .map(|&num| num.to_string())
+                .collect::<Vec<String>>()
+                .join("")
+                .parse::<i32>()
+                .unwrap_or(-1);
+
+            acc + number
+        });
+
+        println!("Result: {}", result);
     }
 }
 
 fn main() {
-    let part_01 = Part01::new(String::from("initial1.txt"), String::from("input1.txt"));
-    part_01.run_tests();
+    let part_01 = Part01::new(String::from("./initial1.txt"), String::from("./input1.txt"));
+    part_01.run(true);
+    part_01.run(false);
 }
